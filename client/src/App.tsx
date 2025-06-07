5d94509d0605c140d184086e6a4512fbb89a0e2d
Benoit Bucher <benoit.bucher@yahoo.fr>
2025-06-07 17:05:11 +0200

    ADD first commit of simplified game without server

---
+++ client/src/App.tsx
@@ -0,0 +1,34 @@
+/**
+ * Main application component that sets up the core routing structure.
+ * This file serves as the root component of the application.
+ */
+
+import { Switch, Route } from "wouter";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+import GamePage from "@/pages/GamePage";
+
+/**
+ * Router component that handles application routing using wouter.
+ * Defines the main routes of the application:
+ * - "/" -> GamePage (main game interface)
+ * - Any other route -> NotFound page
+ */
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={GamePage} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+export default function App() {
+  return (
+    <TooltipProvider>
+      <Router />
+      <Toaster />
+    </TooltipProvider>
+  );
+}
